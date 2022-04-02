41210325b5d7e4fc4e2edaee2d843e1dd3944afc
Kivan <102454385+Kivan1997@users.noreply.github.com>
2022-04-02 19:12:10 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+<script>
+
+function changeImage() {
+var image = document.getElementById('myImage');
+if (image.src.match("bulbon")) {
+image.src = "lightoff.png";
+} else {
+image.src = "lighton.png";
+}
+}
+
+</script>
